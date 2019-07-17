5944e6623ba675c2dbbafa20ac2802855ec7b3c0
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-07-17 11:51:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const cloudinaryUploader = require("../config/cloudinary.js");
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+const app = express();
+
+// Middleware Setup
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+module.exports = app;
